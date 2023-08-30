72d5ade246e3705fa7919ffbc34f544508a248b5
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-08-30 13:23:10 +0100

    adjustable timer working

--- client/sketchwars/src/components/Timer.js
+++ client/sketchwars/src/components/Timer.js
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react'
-
 function Timer() {
-
-  const [ display, setDisplay ] = useState("")
-  const [ totalTime, setTotalTime ] = useState(10)
+  const [ display, setDisplay ] = useState("01:00")
+  const [ totalTime, setTotalTime ] = useState(60)
   const [secsFromInitialStart, setSecsFromInitialStart] = useState(0)
   const [ clock, setClock ] = useState()
   const [ clockPaused, setClockPaused ] = useState(false)
-
-
   const startClockFn = () => {
     const start = new Date()
     let secsFromLastPaused = 0
@@ -16,7 +12,6 @@ function Timer() {
       secsFromLastPaused += secsFromInitialStart
       setClockPaused(false)
     }
-
     setClock(setInterval(() => {   
       let current 
       current = Number(((new Date() - start) / 1000).toFixed())
@@ -28,8 +23,8 @@ function Timer() {
       setDisplay(`${mins}:${secs}`)
     }, 1000))
 }
-
 useEffect(() => {
+  
   if(Number(secsFromInitialStart) === Number(totalTime)) {
     stopClockFn()
   }
@@ -38,19 +33,33 @@ useEffect(() => {
 const stopClockFn = () => {
   clearInterval(clock)
 }
-
 const pauseClockFn = () => {
   setClockPaused(true)
   clearInterval(clock)
 }
 
-const setTimer = (event) => {
-    console.log(typeof event.nativeEvent.data)
-    // const displayCopy = display
-    let newDisplay = display + event.nativeEvent.data
-  setDisplay(newDisplay)
-  let newTotalTime = newDisplay.charAt(3) + newDisplay.charAt(4)
-  setTotalTime(Number(newTotalTime))
+const updateDisplay = (newTotalTime) => {
+  let current = newTotalTime - secsFromInitialStart;
+  let mins = (current / 60).toString().split('.')[0].padStart(2, '0');
+  let secs = (current % 60).toString().padStart(2, '0');
+  setDisplay(`${mins}:${secs}`);
+};
+
+  const add30Secs = () => {
+  setTotalTime(totalTime +30)
+  console.log(totalTime)
+  updateDisplay(totalTime + 30)
+
+}
+
+const minus30Secs = () => {
+  if (totalTime > 0)
+  {setTotalTime(totalTime -30)}
+  else
+  {setTotalTime(totalTime)}
+  console.log(totalTime)
+  updateDisplay(totalTime - 30)
+
 }
 
 return (
@@ -63,7 +72,14 @@ return (
       <button onClick={pauseClockFn}>
         Pause
       </button>
-      <input onChange={setTimer}/>
+      <button className='-30'
+      onClick={minus30Secs}>
+      -30 secs
+      </button>
+      <button className='+30'
+      onClick={add30Secs}>
+      +30 secs
+      </button>
     </div>
   )
 }
