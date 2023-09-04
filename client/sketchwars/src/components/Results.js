2d7ea39a53221fc73f688af935e677910afb3621
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-09-04 19:48:43 +0100

    removed bootstrap

--- client/sketchwars/src/components/Results.js
+++ client/sketchwars/src/components/Results.js
@@ -18,27 +18,7 @@ const Results = () => {
                 <Scoreboard/>
             </div>
             <div className='picturesContainer'>
-                <div id="carouselExample" class="carousel slide">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img src={require("../images/stockDrawing.jpeg")}  class="d-block w-100" alt="pic1"/>
-                        </div>
-                        <div class="carousel-item">
-                        <img src={require("../images/stockDrawing.jpeg")}  class="d-block w-100" alt="pic2"/>
-                        </div>
-                        <div class="carousel-item">
-                        <img src={require("../images/stockDrawing.jpeg")}   class="d-block w-100" alt="pic3"/>
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                    </div>
+                
                 <div className='nameAndDrawing'>
                     <h5>Cambuslang</h5>
                     <h5 style={{ color: 'red' }}>Randolph</h5>
