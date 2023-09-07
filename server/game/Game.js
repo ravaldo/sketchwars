2966abb15a185b53ca57e277452be8357f240a78
ZuhayrK00 <zuhayrk00@gmail.com>
2023-09-07 22:07:46 +0100

    commiting

--- server/game/Game.js
+++ server/game/Game.js
@@ -95,7 +95,115 @@ class Game {
     }
 
 
-   
+    async startGame() {
+        for (this.currentRoundNum; this.currentRoundNum <= this.numOfRounds; this.currentRoundNum++) {
+            console.log(`${this.gameCode} round ${this.currentRoundNum} of ${this.numOfRounds} started`)
+            await this.playRound();
+            console.log(`${this.gameCode} round ${this.currentRoundNum} finished`)
+        }
+        this.endGame();
+    }
+
+
+    * nextPlayer() { // generator
+        const redIterator = this.redTeam[Symbol.iterator]();
+        const blueIterator = this.blueTeam[Symbol.iterator]();
+
+        while (true) {
+            const redPlayer = redIterator.next();
+            if (!redPlayer.done)
+                yield redPlayer.value;
+
+            const bluePlayer = blueIterator.next();
+            if (!bluePlayer.done)
+                yield bluePlayer.value;
+
+            if (redPlayer.done && bluePlayer.done) {
+                break;
+            }
+        }
+    }
+
+
+    async playRound() {
+        const players = this.nextPlayer();
+
+        const playNextTurn = async () => {
+            const player = players.next().value;
+            if (!player)
+                return;
+
+            const word = Game.getRandomWord();
+            this.currentPlayer = player;
+
+            await new Promise((resolve) => {
+
+                const turnFinishedHandler = () => {
+                    console.log(`${this.gameCode} ${player}'s turn finished`);
+                    this.stopTimer();
+                    this.Tablet.off("turnFinished", turnFinishedHandler);
+                    resolve(); // resolve the when the player signals they finished early
+                };
+
+                this.Tablet.on("turnFinished", turnFinishedHandler);
+
+                this.Tablet.emit("turn", player, word, (acknowledgment) => {
+                    if (acknowledgment === "start") {
+                        console.log(`${this.gameCode} ${player} hit start`);
+                        this.startTimer(player, () => {
+                            console.log(`${this.gameCode} ${player}'s turn finished`);
+                            this.stopTimer();
+                            this.Tablet.off("turnFinished", turnFinishedHandler);
+                            resolve(); // resolve the promise when the timer reaches zero
+                        });
+                    } else {
+                        console.error("Invalid acknowledgment");
+                        resolve(); // resolve the promise in case of an error
+                    }
+                });
+            });
+            await playNextTurn();  // continue
+        };
+        await playNextTurn();   // start the first turn
+    }
+
+
+
+
+    endGame() {
+        console.log(this.gameCode + " game ended")
+        this.TV.emit("gameOver")
+        this.Tablet.emit("gameOver")
+        this.status = "RESULTS";
+    }
+
+
+    startTimer(player, callback) {
+        const startTime = Date.now();
+        const timerCallback = () => {
+            if (!this.isPaused) {
+                const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+                console.log(elapsedTime)
+                if (elapsedTime > 10) {
+                    console.log(`${this.gameCode} ${player} ran out of time`);
+                    this.sendState();
+                    callback();
+                } else
+                    this.timer = setTimeout(timerCallback, 1000);
+            }
+        };
+        this.timer = setTimeout(timerCallback, 1000);
+    }
+
+
+    stopTimer() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null;
+        }
+    }
+
+
     sendState() {
         this.TV.emit('gameState', this.toString());
         if (this.Tablet)
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { Server } = require("socket.io");
 const Game = require("./game/Game");
 
 
-allowedOrigins = ["http://localhost:3000", "http://192.168.0.5:3000", "https://sketchwars.vercel.app"]
+allowedOrigins = ["http://localhost:3000", "http://192.168.0.5:3000", "https://sketchwars.vercel.app", "http://192.168.68.62:3000"]
 const app = express();
 app.use(cors({
     origin: allowedOrigins,
